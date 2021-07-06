0c264c6ed3d0348678e8f3fc3251f34532aacae9
koretskiyav <koretskiyav@gmail.com>
2021-07-06 20:50:55 +0300

    add functional components and hooks

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import './index.css';
 
 import App from './components/app';
 import ReactDOM from 'react-dom';
+import { restaurants } from './fixtures';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App restaurants={restaurants} />,
+  document.getElementById('root')
+);
