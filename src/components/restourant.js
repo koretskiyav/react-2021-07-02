199ce9699b56c33f0a67ee9a5994b7f2cfdbda2a
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-07-07 22:12:01 +0300

    add useMemo hook to Restaurant component

--- src/components/restourant.js
+++ src/components/restourant.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
 import Rate from './rate';
@@ -7,9 +8,12 @@ import styles from './restaurant.module.css';
 export default function Restaurant({ restaurant }) {
   const { menu, reviews } = restaurant;
 
-  const commonRating =
-    reviews.reduce((amount, { rating }) => (amount += rating), 0) /
-    reviews.length;
+  const commonRating = useMemo(() => {
+    return (
+      reviews.reduce((amount, { rating }) => (amount += rating), 0) /
+      reviews.length
+    );
+  }, [reviews]);
 
   return (
     <>
@@ -17,7 +21,7 @@ export default function Restaurant({ restaurant }) {
         <h2>Restaurant rating</h2>
         <div className={styles.rating}>
           <Rate rating={Math.round(commonRating)} />
-          <b>{commonRating}</b>
+          <b>{+commonRating.toFixed(1)}</b>
         </div>
       </div>
       <Menu menu={menu} />
