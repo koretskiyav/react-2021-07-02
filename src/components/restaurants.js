6daed4f7e0f6c4d06b92cfe810af06d6a7e73367
Андрей <andrewgul@MacBook-Pro-Teledok.local>
2021-07-08 00:12:06 +0300

    restaurant component

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -1,7 +1,5 @@
 import { useMemo, useState } from 'react';
-
-import Menu from './menu';
-import Reviews from './review/review-list/reviews';
+import Restaurant from './restaurant/restaurant';
 import Tabs from './tabs/tabs';
 
 export default function Restaurants({ restaurants }) {
@@ -20,8 +18,7 @@ export default function Restaurants({ restaurants }) {
   return (
     <div>
       <Tabs tabs={tabs} onChange={setActiveId} />
-      <Menu menu={activeRestaurant.menu} />
-      <Reviews reviews={activeRestaurant.reviews} />
+      <Restaurant restaurant={activeRestaurant} />
     </div>
   );
 }
