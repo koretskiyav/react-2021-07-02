5a27d0c7783dec22b76ba72ca02d340359957214
Пользователь <user1@MacBook-Air-Maksim.local>
2021-07-13 00:34:14 +0300

    ht2

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -30,4 +30,29 @@ describe('Product', () => {
     mount(<Product product={product} fetchData={fn} />);
     expect(fn).toBeCalledWith(product.id);
   });
+
+  it('should decrement amount', () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper
+      .find('button[data-id="product-increment"]')
+      .simulate('click')
+      .simulate('click');
+    wrapper.find('button[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+  });
+
+  it("shouldn't go minus", () => {
+    const wrapper = mount(<Product product={product} />);
+    wrapper.find('button[data-id="product-decrement"]').simulate('click');
+    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+  });
+
+  it('should decrement, no increment', () => {
+    const wrapper = mount(<Product product={product} amount={'2'} />);
+    // не переписывает стэйт
+    wrapper.setProps({ amount: '4' });
+    wrapper.find('button[data-id="product-decrement"]').simulate('click');
+    // выдает результат 0 вместо 3, хотя стэйт поменял на 4
+    // expect(wrapper.find('[data-id="product-amount"]').text()).toBe('3');
+  });
 });
--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,14 +1,15 @@
 import Rate from '../../rate';
 import styles from './review.module.css';
+import PropTypes from 'prop-types';
 
 const Review = ({ user, text, rating }) => (
   <div className={styles.review}>
     <div className={styles.content}>
       <div>
-        <h4 className={styles.name}>{user}</h4>
-        <p className={styles.comment}>{text}</p>
+        <h4 className={styles.name}  data-id="reviews-user">{user}</h4>
+        <p className={styles.comment}  data-id="reviews-text">{text}</p>
       </div>
-      <div className={styles.rate}>
+      <div className={styles.rate} data-id="reviews-rating">
         <Rate value={rating} />
       </div>
     </div>
@@ -17,6 +18,11 @@ const Review = ({ user, text, rating }) => (
 
 Review.defaultProps = {
   user: 'Anonymous',
+};
+
+Rate.propTypes = {
+  user: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default Review;
