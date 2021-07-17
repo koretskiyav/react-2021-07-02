790eb2972eaffa2fe7cd48d0c4fb8e03df46ce19
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-07-17 21:03:48 +0300

    update Basket component - replace reduce to flatMap

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -34,7 +34,7 @@ Basket.propTypes = {
 
 const mapStateToProps = (state, props) => {
   const products = state.restaurants
-    .reduce((acc, restaurant) => acc.concat(restaurant.menu), [])
+    .flatMap(restaurant => restaurant.menu)
     .reduce(
       (acc, product) => (acc = { ...acc, [product.id]: { ...product } }),
       {}
