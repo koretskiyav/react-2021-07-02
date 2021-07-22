2e917f17a9067f255e22109119fc16e5cd2fc38d
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2021-07-22 08:01:41 +0300

    products loading

--- src/api.js
+++ src/api.js
@@ -2,5 +2,6 @@ const get = (url) => fetch(url).then((res) => res.json());
 
 export default {
   loadRestaurants: () => get('/api/restaurants'),
+  loadProducts: (restId) => get(`/api/products?id=${restId}`),
   loadReviews: (restId) => get(`/api/reviews?id=${restId}`),
 };
--- src/redux/features/products.js
+++ src/redux/features/products.js
@@ -1,15 +1,45 @@
-import { normalizedProducts } from '../../fixtures';
 import { arrToMap } from '../utils';
+import api from '../../api';
+import { REQUEST, SUCCESS, FAILURE } from '../constants';
 
-export default (state = arrToMap(normalizedProducts), action) => {
-  const { type } = action;
+export const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+
+export const loadProducts = (restId) => ({
+  type: LOAD_PRODUCTS,
+  meta: {
+    apiCall: () => api.loadProducts(restId),
+  },
+  restId,
+});
+
+const initialValue = {
+  productsWasLoadedFor: {},
+  entities: {},
+  error: null,
+};
+
+export default (state = initialValue, action) => {
+  const { type, payload, restId, error } = action;
 
   switch (type) {
+    case LOAD_PRODUCTS + REQUEST:
+      return state;
+    case LOAD_PRODUCTS + SUCCESS:
+      return {
+        ...state,
+        productsWasLoadedFor: { ...state.productsWasLoadedFor, [restId]: true },
+        entities: { ...state.entities, ...arrToMap(payload) },
+      };
+    case LOAD_PRODUCTS + FAILURE:
+      return { ...state, error: error };
     default:
       return state;
   }
 };
 
-export const productsSelector = (state) => state.products;
+export const productsSelector = (state) => state.products.entities;
+
+export const loadingProductsSelector = (state, props) =>
+  !state.products.productsWasLoadedFor[props.restId];
 
 export const productSelector = (state, { id }) => productsSelector(state)[id];
