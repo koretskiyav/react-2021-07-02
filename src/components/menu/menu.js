576c158f23674cb200bb819fec63b02ab8f62f25
Пользователь <user1@MacBook-Air-Maksim.local>
2021-07-22 22:38:50 +0300

    HT5

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,42 +1,47 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 import Product from '../product';
 import Basket from '../basket';
+import { connect } from 'react-redux';
+import { loadProducts } from '../../redux/features/products';
+import Loader from '../loader';
+import { productsIsLoadingSelector } from '../../redux/selectors';
 
 import styles from './menu.module.css';
 
-class Menu extends Component {
-  static propTypes = {
-    menu: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-  };
+const Menu = ({ menu, restId, loadProducts, isLoading }) => {
+  useEffect(() => {
+    !isLoading && loadProducts(restId);
+  }, [isLoading, loadProducts, restId]);
 
-  state = { error: null };
+  if (!isLoading) return <Loader />;
 
-  componentDidCatch(error) {
-    this.setState({ error });
-  }
-
-  render() {
-    const { menu } = this.props;
-
-    if (this.state.error) {
-      return <p>Меню этого ресторана сейчас недоступно :(</p>;
-    }
-
-    return (
-      <div className={styles.menu}>
-        <div>
-          {menu.map((id) => (
-            <Product key={id} id={id} />
-          ))}
-        </div>
-        <div>
-          <Basket />
-        </div>
+  return (
+    <div className={styles.menu}>
+      <div>
+        {menu.map((id) => (
+          <Product key={id} id={id} />
+        ))}
       </div>
-    );
-  }
-}
+      <div>
+        <Basket />
+      </div>
+    </div>
+  );
+};
 
-export default Menu;
+Menu.propTypes = {
+  menu: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  isLoading: PropTypes.bool,
+};
+
+const mapStateToProps = (state, props) => ({
+  isLoading: productsIsLoadingSelector(state, props),
+});
+
+const mapDispatchToProps = {
+  loadProducts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
