f0d75a6c5ea22b136c2b6a9fdcd789bfa21c10fc
koretskiyav <koretskiyav@gmail.com>
2021-07-23 20:22:08 +0300

    add createReducer

--- src/redux/features/order.js
+++ src/redux/features/order.js
@@ -1,22 +1,23 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 export const increment = createAction('order/increment');
 export const decrement = createAction('order/decrement');
 export const remove = createAction('order/remove');
 
-export default (state = {}, action) => {
-  const { type, payload: id } = action;
-  switch (type) {
-    case increment.type:
-      return { ...state, [id]: (state[id] || 0) + 1 };
-    case decrement.type:
-      return { ...state, [id]: state[id] > 0 ? (state[id] || 0) - 1 : 0 };
-    case remove.type:
-      return { ...state, [id]: 0 };
-    default:
-      return state;
+export default createReducer(
+  {},
+  {
+    [increment]: (state, { payload: id }) => {
+      state[id] = (state[id] || 0) + 1;
+    },
+    [decrement]: (state, { payload: id }) => {
+      state[id] = state[id] > 0 ? (state[id] || 0) - 1 : 0;
+    },
+    [remove]: (state, { payload: id }) => {
+      state[id] = 0;
+    },
   }
-};
+);
 
 export const orderSelector = (state) => state.order;
 
